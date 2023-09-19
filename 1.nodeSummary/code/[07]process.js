432cf44758960700c1d6b75e3db33d29fcbc97f9
naginooi <xxxkmsz@naver.com>
2023-09-19 23:55:23 +0900

    process

---
+++ 1.nodeSummary/code/[07]process.js
@@ -0,0 +1,34 @@
+const process = require('process');
+
+process.version; // 버전
+process.arch; // 아키텍처 정보
+process.platform; // 운영체제 플랫폼 정보
+process.pid; // 현재 프로세스의 아이디
+process.uptime(); // 프로세스가 시작된 후 흐른 시간 (단위: 초)
+process.execPath; // 노드의 경로
+process.cwd(); // 현재 프로세스가 실행되는 위치
+process.cpuUsage(); // 현재 cpu 사용량
+
+// process.env --> 시스템 환경 변수를 나타냄
+
+setImmediate(() => {
+  console.log('immediate');
+});
+
+//이벤트 루프가 다른 콜백 함수들보다 nextTick의 콜백 함수를 우선으로 처리
+process.nextTick(() => {
+  console.log('nextTick');
+});
+
+setTimeout(() => {
+  console.log('timeout');
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log('promise');
+});
+
+// nextTick
+// promise --> resolve된 promise도 다른 콜백들보다 우선시 됨
+// timeout
+// immediate
