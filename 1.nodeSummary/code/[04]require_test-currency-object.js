715d491383a513fa88d9dd25d9dc750e247a3ab9
naginooi <xxxkmsz@naver.com>
2023-09-19 22:13:54 +0900

    module.export

--- 1.nodeSummary/code/[04]require_test-currency-object.js
+++ 1.nodeSummary/code/[04]require_test-currency-object.js
@@ -1,5 +1,7 @@
-const counter = require("./4_counter.js");
-counter.increase();
-counter.increase();
-counter.increase();
-console.log(counter.getCount());
+const currency = require('./[04]require_currency-object');
+
+console.log('50 Canadian dollars equals this amount of US dollars:');
+console.log(currency.canadianToUs(50));
+
+console.log('30 Canadian dollars equals this amount of Canadian dollars:');
+console.log(currency.usToCanadian(30));
