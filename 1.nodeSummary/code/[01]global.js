86b3ba92526dd33fd993f147ba62b16f77650dbe
naginooi <xxxkmsz@naver.com>
2023-09-19 23:16:18 +0900

    global

--- 1.nodeSummary/code/[01]global.js
+++ 1.nodeSummary/code/[01]global.js
@@ -1,3 +1,7 @@
+//node.js에 내장되어 있는 fs 모듈 불러오기 : 파일 입출력 처리할 때 사용
 const fs = require('fs');
-console.log('hello');
+//import fs from 'fs';
+
 global.console.log('hello');
+
+console.log('hello'); // 글로벌 생략 가능 !
