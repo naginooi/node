44cc0258238da017386df54f165ddf076be1582f
naginooi <xxxkmsz@naver.com>
2023-09-19 23:43:53 +0900

    timer

--- 1.nodeSummary/code/8_interval.js
+++
@@ -1,9 +0,0 @@
-let num = 1;
-const interval = setInterval(() => {
-  //let num = 1 --> 블럭 안에 있어서 계속 초기화됨
-  console.log(num++);
-}, 1000);
-
-setTimeout(() => {
-  clearInterval(interval);
-}, 6000);
