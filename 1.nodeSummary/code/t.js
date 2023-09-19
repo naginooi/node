ace0a021c181555db62d44ce1e95d7c3c0a7c8e2
naginooi <xxxkmsz@naver.com>
2023-09-19 22:07:28 +0900

    c

---
+++ 1.nodeSummary/code/t.js
@@ -0,0 +1,19 @@
+/* 단일 객체 내보내기 */
+
+const exchangeRate = 0.91;
+
+//안 내보냄
+function roundTwoDecimals(amount) {
+  return Math.round(amount * 100) / 100;
+}
+
+//내보내기
+const obj = {};
+obj.canadianToUs = function (canadian) {
+  return roundTwoDecimals(canadian * exchangeRate);
+};
+obj.usToCanadian = function (american) {
+  return roundTwoDecimals(american / exchangeRate);
+};
+module.exports = obj;
+module;
